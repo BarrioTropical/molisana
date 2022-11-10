82f20aae7d632f1b724afa1b1adab9b36c2bf921
BarrioTropical <barriotropical@gmail.com>
2022-11-10 11:50:24 +0100

    creazione componenti e inserimento img folder
    Signed-off-by: BarrioTropical <“barriotropical@gmail.com”>

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HeaderComponent/>
+
+  <main>
+  <ProductList/>
+  </main>
+
+  <FooterComponent/>
+
+</template>
+
+<script>
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import ProductList from './components/ProductList.vue';
+
+  export default {
+    components: { HeaderComponent, ProductList, FooterComponent }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+    Sono Lista Prodotti
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'ProductList'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
